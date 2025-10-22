85469be1e334eff2b23bd484397e439d08af18bb
mahdawemghr <mahdemghr2003@gmail.com>
2025-10-22 21:28:23 +0300

    ready

---
+++ lib/components/movie_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movie.dart';
+import 'package:movie_app/pages/movie_deatials.dart';
+
+Card movieCard(Movie movie, BuildContext context, bool flag) {
+  return Card(
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ExpansionTile(
+        title: Text(movie.title),
+        subtitle: Text('Director: ${movie.director}'),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(movie.poster),
+          onBackgroundImageError: (_, __) {},
+          child: Text(movie.title[0]),
+        ),
+
+        children: [
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(left: 70),
+            child: Column(
+              children: [
+                RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: [
+                      TextSpan(
+                        text: 'Released: ',
+                        style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: movie.released,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      TextSpan(
+                        text: '\nPlot: ',
+                        style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: movie.plot,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                ),
+                if (flag)
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDeatials(movie: movie),
+                        ),
+                      );
+                    },
+                    child: Text('Read More'),
+                  )
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
--- lib/pages/movie_deatials.dart
+++ lib/pages/movie_deatials.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/components/movie_card.dart';
+import 'package:movie_app/models/movie.dart';
 
 class MovieDeatials extends StatelessWidget {
-  const MovieDeatials({super.key});
+  final Movie movie;
+  const MovieDeatials({super.key, required this.movie});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        title: Text('Movie Deaails'),
+        title: Text('Movie Details'),
+      ),
+      body: Column(
+        children: [
+          movieCard(movie, context, false),
+          Text(
+            'Movie images',
+            style: Theme.of(
+              context,
+            ).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          Image(
+            image: NetworkImage(movie.images),
+            errorBuilder: (context, error, stackTrace) =>
+                const SizedBox.shrink(),
+          ),
+        ],
       ),
     );
   }
